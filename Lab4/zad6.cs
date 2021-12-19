984f4dc9a8737f0ec501ba9e6dab637dbda8ecc3
damw32 <47741103+damw32@users.noreply.github.com>
2021-12-19 23:10:05 +0100

    Add files via upload

---
+++ Lab4/zad6.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad6 : MonoBehaviour
+{
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Contact")
+        {
+            Debug.Log("Doszło do kontaktu");
+        }
+    }
+}
---
+++ Lab5/zad3.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad3 : MonoBehaviour
+{
+    public float elevatorSpeed = 2f;
+    private bool isRunning = false;
+    private bool isRunningBack = false;
+    private int indexPosition = 0;
+    private Vector3 StartPosition;
+    public List<Vector3> EndPositions;
+
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+
+        if (isRunning)
+        {
+            float step = elevatorSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, EndPositions[indexPosition], step);
+            if (transform.position == EndPositions[indexPosition])
+            {
+                if (indexPosition == EndPositions.Count - 1)
+                {
+                    isRunning = false;
+                    isRunningBack = true;
+                }
+                else
+                {
+                    indexPosition++;
+                }
+
+            }
+        }
+        if (isRunningBack)
+        {
+            float step = elevatorSpeed * Time.deltaTime;
+            if (indexPosition > 0)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, EndPositions[indexPosition], step);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, StartPosition, step);
+            }
+
+            if (transform.position == EndPositions[indexPosition])
+            {
+                indexPosition--;
+            }
+            if (transform.position == StartPosition)
+            {
+                isRunningBack = false;
+            }
+
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (transform.position == StartPosition)
+            {
+                isRunning = true;
+            }
+        }
+    }
+}
